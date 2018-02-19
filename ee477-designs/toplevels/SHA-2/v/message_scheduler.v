0b4d5aafd76b987b656a5d55b2c3429a9befcff1
jichunli <jichunli@uw.edu>
2018-02-19 01:39:53 -0800

    Start message_scheduler

---
+++ ee477-designs/toplevels/SHA-2/v/message_scheduler.v
@@ -0,0 +1,9 @@
+// This is the message scheduler module for SHA-256 accelerator which produces the Wt value for SHA-256 digest
+//
+module message scheduler
+	(input 
+	,input
+	,output
+	);
+
+endmodule 
