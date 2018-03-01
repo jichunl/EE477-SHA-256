79fb8a06f6b8255474e23b87dd3f892a502956ea
boora-bee <36610261+boora-bee@users.noreply.github.com>
2018-03-01 15:33:05 -0800

    bug fix

--- SHA256_node.v
+++ SHA256_node.v
@@ -13,43 +13,56 @@ module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 	,input				yumi_i
 	,input	[ring_width_p-1:0] 	data_i
 	,output	logic			ready_o
-	,output logic				v_o
-	//,output	[255:0]			data_o
+	,output logic			v_o
 	,output [ring_width_p-1:0]	data_o
 	);							
 
 	logic	core_ready_o, core_v_i, core_yumi_i, core_v_o, core_en_i
-		,assembler_v_i, assembler_v_o,assembler_ready_o, assembler_en_i, assembler_yumi_i;
-	reg	[255:0]	assembler_data_o;
+		,assembler_v_i, assembler_v_o,assembler_ready_o, assembler_en_i, assembler_yumi_i, deassembler_en_i, deassembler_v_o, deassembler_ready_o, deassembler_v_i;
+	reg	[255:0]	assembler_data_o, core_data_o;
 //		assembler_v_i <= v_i;
 //		assembler_v_o <= v_o;
 //		assembler_ready_o <= ready_o;			
+	
 
 	bsg_assembler
-	#(.ring_width_p( ring_width_p), .id_p(id_p))assembler 
+		#(.ring_width_p( ring_width_p), .id_p(id_p))assembler 
 				(.clk_i(clk_i)
 				,.reset_i(reset_i)
 				,.en_i(assembler_en_i)
-				,.v_i(assembler_v_i)
+				,.v_i(v_i)
 				,.data_i(data_i)
 				,.ready_o(assembler_ready_o)
 				,.v_o(assembler_v_o)
 				,.data_o(assembler_data_o)
-				,.yumi_i(core_v_o)
+				,.yumi_i(core_ready_o)
 				);
 	SHA256_core
-		#(.ring_width_p( ring_width_p), .id_p(id_p))
-	core
-				(.clk_i(clk_i)
+		#(.ring_width_p( ring_width_p), .id_p(id_p))core
+			(.clk_i(clk_i)
 				,.reset_i(reset_i)
 				,.en_i(core_en_i)
-				,.v_i(core_v_i)
-				,.yumi_i(core_yumi_i)
+				,.v_i(assembler_v_o)
+				,.yumi_i(deassembler_ready_o)
 				,.msg_i(assembler_data_o)
 				,.ready_o(core_ready_o)
 				,.v_o(core_v_o)
-				,.digest_o(data_o)
+				,.digest_o(core_data_o)
 				);
+
+   bsg_deassembler
+                #(.ring_width_p( ring_width_p), .id_p(id_p))deassembler
+                        (.clk_i(clk_i)
+                                ,.reset_i(reset_i)
+                                ,.en_i(deassembler_en_i)
+                                ,.v_i(deassembler_v_i)
+                                ,.yumi_i(yumi_i)
+                                ,.data_i(core_data_o)
+                                ,.ready_o(deassembler_ready_o)
+                                ,.v_o(deassembler_v_o)
+                                ,.data_o(data_o)
+                                );
+	
 
 	localparam WAIT	= 2'b00;
 	localparam CALC	= 2'b01;
@@ -70,21 +83,33 @@ module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 			WAIT: begin
 				assembler_en_i = 1'b1;
 				core_en_i = 1'b0;
+				deassembler_en_i = 1'b0;
+				ready_o = 1'b1;
+				v_o = 1'b0;
 			end
 			CALC: begin
-				if(assembler_v_o==1'b1) begin
+				ready_o = 1'b0;
+				v_o = 1'b0;
+				if(assembler_v_o==1'b1) 
+				begin
 					core_en_i = 1'b1;
 					assembler_en_i = 1'b0;
-				end else begin
-					assembler_en_i = 1'b1;		
-					core_en_i = 1'b0;
+					deassembler_en_i = 1'b0;
+					if(core_v_o == 1'b1)
+					begin
+						
+						core_en_i = 1'b0;
+						deassembler_en_i = 1'b1;
+						assembler_en_i = 1'b0;	
+					end 
 				end
 			end
 			DONE: begin
 				ready_o = 1'b0;
-				v_o	= 1'b0;
+				v_o	= 1'b1;
 				assembler_en_i = 1'b0;
 				core_en_i = 1'b0;
+				deassembler_en_i = 1'b1;
 			end
 		endcase
 	end
