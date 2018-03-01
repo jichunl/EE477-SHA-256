93e19d5b55da85286331247b2820ffd4428cc7ad
boora-bee <36610261+boora-bee@users.noreply.github.com>
2018-02-28 20:29:34 -0800

    added deassembler, bug fixes

--- ee477-designs/toplevels/SHA256/v/SHA256_node.v
+++ ee477-designs/toplevels/SHA256/v/SHA256_node.v
@@ -3,25 +3,8 @@
 //
 //24-feb-18, added state machine, changed variable names
 //
-// This is the wrapper module for a single SHA256 node there are two parts the
-// SHA256_core and assembler
-//
-// Comments on update:
-// 	N/A
-//
-// input:
-// 	clk_i		
-// 	reset_i		
-// 	en_i
-// 	v_i		:indicates that input signal is valid
-// 	yumi_i		:indicates that the outside world has got the data
-//
-// output:
-// 	ready_o		:indicates that this module is ready for new data
-//	v_o		:indicates that the output data is valid
-//	data_o		
-//
-// Last modified on: Tue Feb 27 16:13:00 2018
+//29-feb-18 bug fixes
+//known issues- vcs error, deassembler
 module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 	(input				clk_i
 	,input				reset_i
@@ -31,30 +14,32 @@ module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 	,input	[ring_width_p-1:0] 	data_i
 	,output	logic			ready_o
 	,output logic			v_o
-	,output	[255:0]			data_o
+	,output [ring_width_p-1:0]	data_o
 	);							
 
 	logic	core_ready_o, core_v_i, core_yumi_i, core_v_o, core_en_i
-		,assembler_v_i,	assembler_v_o, assembler_ready_o, assembler_en_i;
-	reg	[255:0]	assembler_data_o;
-	assign	assembler_v_i = v_i;
-	assign	assembler_v_o = v_o;
-	assign	assembler_ready_o = ready_o;			
+		,assembler_v_i, assembler_v_o,assembler_ready_o, assembler_en_i, assembler_yumi_i, deassembler_en_i, deassembler_v_o, deassembler_ready_o, deassembler_v_i;
+	reg	[255:0]	assembler_data_o, core_data_o;
+//		assembler_v_i <= v_i;
+//		assembler_v_o <= v_o;
+//		assembler_ready_o <= ready_o;			
+	
 
 	bsg_assembler
-		assembler 	(.clk_i(clk_i)
+		#(.ring_width_p( ring_width_p), .id_p(id_p))assembler 
+				(.clk_i(clk_i)
 				,.reset_i(reset_i)
 				,.en_i(assembler_en_i)
-				,.v_i(assembler_v_i)
+				,.v_i(v_i)
 				,.data_i(data_i)
 				,.ready_o(assembler_ready_o)
 				,.v_o(assembler_v_o)
 				,.data_o(assembler_data_o)
-				,.yumi_i(assembler_yumi_i)
+				,.yumi_i(core_v_o)
 				);
 	SHA256_core
-		core
-				(.clk_i(clk_i)
+		#(.ring_width_p( ring_width_p), .id_p(id_p))core
+			(.clk_i(clk_i)
 				,.reset_i(reset_i)
 				,.en_i(core_en_i)
 				,.v_i(core_v_i)
@@ -62,8 +47,22 @@ module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 				,.msg_i(assembler_data_o)
 				,.ready_o(core_ready_o)
 				,.v_o(core_v_o)
-				,.digest_o(data_o)
+				,.digest_o(core_data_o)
 				);
+
+   bsg_deassembler
+                #(.ring_width_p( ring_width_p), .id_p(id_p))deassembler
+                        (.clk_i(clk_i)
+                                ,.reset_i(reset_i)
+                                ,.en_i(deassembler_en_i)
+                                ,.v_i(deassembler_v_i)
+                                ,.yumi_i(yumi_i)
+                                ,.data_i(core_data_o)
+                                ,.ready_o(deassembler_ready_o)
+                                ,.v_o(deassembler_v_o)
+                                ,.data_o(data_o)
+                                );
+	
 
 	localparam WAIT	= 2'b00;
 	localparam CALC	= 2'b01;
@@ -84,21 +83,33 @@ module SHA256_node #(parameter ring_width_p = "inv", parameter id_p="inv")
 			WAIT: begin
 				assembler_en_i = 1'b1;
 				core_en_i = 1'b0;
+				deassembler_en_i = 1'b0;
+				ready_o = 1'b1;
+				v_o = 1'b0;
 			end
 			CALC: begin
-				if(assembler_v_o==1'b1) begin
+				ready_o = 1'b0;
+				v_o = 1'b0;
+				if(assembler_v_o==1'b1) 
+				begin
 					core_en_i = 1'b1;
 					assembler_en_i = 1'b0;
-				end else begin
-					assembler_en_i = 1'b1;		
-					core_en_i = 1'b0;
+					deassembler_en_i = 1'b0;
+					if(core_v_o == 1'b1)
+					begin
+						
+						core_en_i = 1'b0;
+						deassembler_en_i = 1'b1;
+						assembler_en_i = 1'b0;	
+					end 
 				end
 			end
 			DONE: begin
 				ready_o = 1'b0;
-				v_o	= 1'b0;
+				v_o	= 1'b1;
 				assembler_en_i = 1'b0;
 				core_en_i = 1'b0;
+				deassembler_en_i = 1'b1;
 			end
 		endcase
 	end
