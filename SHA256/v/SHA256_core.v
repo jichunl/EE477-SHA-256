521c5ab60e951dac1e4c8f01b9c67152687fc24a
Jichun Li <jichunli@uw.edu>
2018-03-01 22:05:00 -0800

    fixed a problem with bsg_assembler output, details listed in the comments

--- SHA256/v/SHA256_core.v
+++ SHA256/v/SHA256_core.v
@@ -2,6 +2,9 @@
 // message scheduler and compression together
 //
 // Comment on update:
+// 	remove v_r, v_n and assign v_o directly in FSM; changed cycle_counter
+// 	update logic
+// 	------------------------------------------------------------------------
 // 	create a dff for cycle_counter to deal with ICSD error
 //	add 2 new control signal ctr_reset (cycle_conter_reset) and ctr_en
 //	(cycle_counter_enable)
@@ -19,7 +22,7 @@
 // 	v_o:		indicates that this module has produced valid outputfe
 // 	digest_o:	the result of hashing
 //
-// Last modified on: Tue Feb 27 15:54:07 2018
+// Last modified on: Thu Mar  1 21:24:16 2018
 module SHA256_core #(parameter core_id = "inv")
 	(input 				clk_i
 	,input 				reset_i
@@ -50,7 +53,7 @@ module SHA256_core #(parameter core_id = "inv")
 	reg 	[255:0]    	digest_r;
 	reg	[31:0]		Kt_r;
 	reg	[31:0]		Wt_r;
-logic ctr_en;	
+	logic ctr_en;	
 	// state
 	typedef enum [1:0] {eWait, eBusy, eDone} state_e;
 	state_e state_n, state_r;	
@@ -59,12 +62,10 @@ logic ctr_en;
 	reg	[255:0]		msg_r, msg_n;
 	reg	[511:0]		block;
 	// control logic
-//	reg			v_r, v_n;
-//	assign v_o = v_r;
 
 	reg 		 ctr_reset;
-	reg	[7:0]	cycle_counter;//, cycle_counter_n;
-//	assign cycle_counter_n = cycle_counter + 1'b1;
+	reg	[6:0]	cycle_counter;
+ 
 	
 	reg msg_sch_init;
 	assign  msg_sch_init = (state_n == eBusy);
@@ -99,18 +100,12 @@ logic ctr_en;
 		if (reset_i) begin
 			state_r <= eWait;
 			msg_r <= 256'b0;
-		//	v_r <= 1'b0;
 		end else begin
 			state_r <= state_n;
 			msg_r <= msg_n;
-		//	v_r <= v_n;
 		end
 	end
 	
-//	always_ff @(posedge clk_i) begin
-//		 cycle_counter = (ctr_reset | reset_i) ? 6'b0:cycle_counter_n;
-//	end
-
 
     always_ff @(posedge clk_i) 
 	begin
@@ -133,7 +128,6 @@ logic ctr_en;
 			if (v_i==1'b1) begin
 					state_n = eBusy;
 					msg_n = msg_init;
-			//		v_n = 1'b0;
 					ctr_reset = 1'b1;
 				end
 			end
@@ -145,13 +139,10 @@ logic ctr_en;
 				if (cycle_counter == 7'b1000000) begin
 					state_n = eDone;
 					digest_o = digest_r;
-			//		v_n = 1'b1;
-					  ready_o = 1'b0;
+					ready_o = 1'b0;
                                         v_o = 1'b0;
-//					ctr_reset=1'b0;
 				end else begin
 					state_n = eBusy;
-			//		v_n = 1'b0;
 					msg_n = digest_r;
 					    ready_o = 1'b0;
                                         v_o = 1'b0;
@@ -165,7 +156,7 @@ logic ctr_en;
 					state_n = eWait;
 					ctr_en = 1'b0;
 					ctr_reset = 1'b1;
-					      ready_o = 1'b0;
+					ready_o = 1'b0;
                                         v_o = 1'b1;
 
 				end
@@ -176,7 +167,4 @@ logic ctr_en;
 			end
 		endcase
 	end
-
-
-
 endmodule
