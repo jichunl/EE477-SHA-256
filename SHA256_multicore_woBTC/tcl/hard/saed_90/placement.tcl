66545beee4265205b8a04a9d0afcf2f4c4790b26
boora-bee <36610261+boora-bee@users.noreply.github.com>
2018-04-17 16:14:42 -0700

    Delete placement.tcl

--- SHA256_multicore_woBTC/tcl/hard/saed_90/placement.tcl
+++
@@ -1,7 +0,0 @@
-# placement.tcl
-#
-# This file contains information used by icc to specify placement of
-# cell groups or blocks within the floorplan of the chip. This is quite
-# difficult to make it prcess independant thus this file is in the
-# hardened folder.
-#
